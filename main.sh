6b2bf0730508926f23caeb97343fb484a76a20f7
Carlo Cabada <ccabada2@miners.utep.edu>
2019-10-23 02:00:56 -0700

    Need to fix decimal to ascii

--- main.sh
+++ main.sh
@@ -2,23 +2,34 @@
 echo "Bash Assignment"
 echo "Enter filename"
 #read filename
-filename="innocuous.txt"
+filename="mini-fishlist.txt"
+#filename="innocuous.txt"
 echo "Enter codebook"
 #read codebook
-codebook="codebook.txt"
-output="out.txt"
+#codebook="codebook.txt"
+codebook="mini-codebook.txt"
 
 
-while read line;
-do
-    if [ $((`echo "$line"|awk '{ print $2 }'` %2)) -eq 0 ];
-    then echo "$line";
-    fi ;
-done < "$filename"
 
-#echo Sorts by the second column
-#awk {'print $2 %2 '} $filename | sort -n > sorted.txt
-#echo Replaces dash with space
+echo Sorts by the second column
+awk {'print $2 '} $filename | sort -n > sorted.txt
+cat sorted.txt
+
+echo Replaces dash with space
 cat sorted.txt | sed 's/-/ /g' > sorted_no_dashes.txt
-#echo Moves second column to another file
-awk {'print $2'} sorted_no_dashes.txt > ascii.txt
+cat sorted_no_dashes.txt
+
+#Error 
+echo Moves second column to another file
+awk {'print ($2 + 3)'}  sorted_no_dashes.txt > ascii.txt
+
+echo ascii
+cat ascii.txt
+join  ascii.txt $codebook -a 1
+echo res
+#cat $result
+
+
+echo reading file
+
+
